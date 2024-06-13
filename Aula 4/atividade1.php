79b5dcd5514485dc4d9bf51c67e3bf41b4f66d14
Lucas Martins Bezerra <titannytronel@gmail.com>
2024-06-13 20:25:05 -0300

    atividade4

---
+++ Aula 4/atividade1.php
@@ -0,0 +1,17 @@
+<html>
+    <body>
+        <?php
+            $nome = "Till Lindesma";
+               if($nome == 'Alex') {
+                echo 'É o alex';
+             } elseif ($nome == 'Miranda') {
+                echo 'É o Miranda' ;
+             } elseif ($nome == "Till Lindesma") {
+                echo 'acertou mizerave' ;
+             } else {
+                echo 'Você é um impostor';
+
+             }
+        ?>
+    </body>
+</html>
