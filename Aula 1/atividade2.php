52978547531e31ae43ae99632da2c4a7ee4de46b
Lucas Martins Bezerra <titannytronel@gmail.com>
2024-06-25 20:51:46 -0300

    Atividade2

---
+++ Aula 1/atividade2.php
@@ -0,0 +1,32 @@
+<html>
+    <body>
+        <?php
+            $listaAlunoIdade = array(
+                "Leonardo" => 22,
+                "Alemão" => 32,
+                "Cabelludo" => 15,
+            );
+            $listaCores = array(
+                "vemelho",
+                "laranja",
+                "cinza",
+                "marrom"
+            );
+
+            echo $listaAlunoIdade["Cabeludo"];
+
+            echo "br />";
+
+            echo $listaCores[2];
+
+            echo "<br />";
+            
+            var_dump($listaAlunoIdade);
+
+            echo "<br />";
+
+            var_dump($listaCores);
+
+        ?>
+    </body>
+</html>
