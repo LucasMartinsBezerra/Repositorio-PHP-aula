ecd36b5f4d6fb0f03b0d8239a0ecf5d20a726334
Lucas Martins Bezerra <titannytronel@gmail.com>
2024-06-27 20:29:21 -0300

    Atividade Estruturas de Decisões

---
+++ Aula 4/atividade5.php
@@ -0,0 +1,17 @@
+<?php
+    $hoje = "Quarta";
+    Switch($hoje){
+        case "Segunda";
+            echo "Hoje é segunda feira";
+            break;
+        case "Terça";
+            echo "Hoje é terça feira";
+            break;
+        case "Quarta";
+            echo "Hoje é quata feira";
+            break;
+        default:
+            echo "Não é inicio da semana";
+
+    }
+?>
